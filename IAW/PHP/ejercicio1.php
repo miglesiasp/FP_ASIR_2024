650fa97e4438161c6d2fbbb606e1488d7eaa88d1
Miguel Iglesias <miglesiasp05@suarezdefigueroa.es>
2024-11-28 13:50:17 +0100

    php ejercicio 1

---
+++ IAW/PHP/ejercicio1.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <h1> ¿Donde se ejecuta el código PHP</h1>
+        <p> En el servidor</p>
+
+    <h1>¿Qué tipo de contenido puede generar PHP?</h1>
+        <?php
+        echo "<p>Con PHP se puede trabajar de forma muy amplia, ya que 
+            al ser de acceso libre y popular, cuenta con muchas extensiones 
+            distintas. Con PHP se puede trabajar con cookies, procesar formularios, 
+            generar contenidos dinámicos, CGI, acceso a sistemas de almacenamiento, etc.
+        </p>";
+        ?>
+        
+    <h1>¿Cuál es la diferencia entre PHP y JavaScript en términos de dónde se ejecutan?</h1>
+        <?php
+        echo "<p>La diferencia entre PHP y JavaScript es que, mientras que PHP 
+            se ejecuta en el servidor, JavasScript, al igual que HTML y CSS, 
+            se ejecuta en el lado del cliente.
+        </p>";
+        ?>
+        </body>
+</html>
