9859a7585ecb8cb29ba108c7157eb98064ceb5e9
Miguel Iglesias <miglesiasp05@suarezdefigueroa.es>
2024-12-03 14:10:38 +0100

    ejercicios

---
+++ IAW/PHP/EJERCICIO_PHP2/ejercicio7.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 7</title>
+</head>
+<body>
+    <?php
+    $mult = 1234 * 532;
+
+    echo $mult;
+    ?>
+        
+</html>
